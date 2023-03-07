c9fe7de135a809fb28a65f47617da9a74f3e55f9
re-doc <xqcokid@gmail.com>
2023-03-07 17:21:36 +0800

    every 3rd cube rotate over time

--- myopengl/Main.cpp
+++ myopengl/Main.cpp
@@ -251,7 +251,9 @@ int main()
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 1.3f, 0.5f));
+            if (i % 3 == 0)  // every 3rd iteration (including the first) we set the angle using GLFW's time function.
+                angle = glfwGetTime() * 25.0f;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.0f, 0.0f));
             ourShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
